9c5f99e86890c84e8a68464890ae1a078077bbbc
Dani Lorenzo <danilorenzo407@gmail.com>
2017-01-30 20:57:34 +0100

    Añadido nombres de las pestañas del menu

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -36,9 +36,8 @@ AppAsset::register($this);
     echo Nav::widget([
         'options' => ['class' => 'navbar-nav navbar-right'],
         'items' => [
-            ['label' => 'Home', 'url' => ['/site/index']],
-            ['label' => 'About', 'url' => ['/site/about']],
-            ['label' => 'Contact', 'url' => ['/site/contact']],
+            ['label' => 'Portada', 'url' => ['/site/index']],
+            ['label' => 'Contacto', 'url' => ['/site/contact']],
             Yii::$app->user->isGuest ? (
                 ['label' => 'Login', 'url' => ['/site/login']]
             ) : (
